dd940dc942da9be1c447557fb2d25596d78b8630
Sely2k <sely2k@gmail.com>
2021-10-12 18:40:50 +0200

    fix

---
+++ DevOps Conf/2021/Banners/banner-data.jsx
@@ -0,0 +1,263 @@
+var sessions = [
+    {
+        "roomnumber" : "3",
+
+        "sessiontime": "09:00",
+        "speakername" : "Alessandro Melchiori",
+        "speakerrole" : "Software developer @ CodicePlastico",
+        "sessionlanguage" : "IT",
+        "sessionhashtag" : "#inspiration",
+        "sessiontitle" :"GitHub Actions e Terraform: l'infrastruttura, dalla definizione al deploy",
+        filename : "/thumb/Alessandro_Melchiori.png",
+        "rebuild": true,
+        "save": true
+    },
+    {
+        roomnumber : "1",
+
+        sessiontime: "09:40",
+        speakername : "Alpa Buddhabhatti",
+        speakerrole : "Cluster reply , London , UK",
+        sessionlanguage : "",
+        sessionhashtag : "",
+        sessiontitle :"Azure SQL Server & DevOps",
+        filename : "/thumb/Alpa_Buddhabhatti.jpg",
+        rebuild: true,
+        save: true
+    },
+    {
+        roomnumber : "2",
+        sessiontime: "09:40",
+        speakername : "Andrea Carratta",
+        speakerrole : "Senior Backend & Cloud Azure Developer",
+        sessionlanguage : "",
+        sessionhashtag : "",
+        sessiontitle :"Azure DevOPS: dal repository al NuGET",
+        filename : "/thumb/Andrea_Carratta.jfif",
+        rebuild: true,
+        save: true
+    },
+    {
+        roomnumber : "4",
+        sessiontime: "09:40",
+        speakername : "Antonio Liccardi",
+        speakerrole : "Chief Technology Officer @ Blexin s.r.l.",
+        sessionlanguage : "",
+        sessionhashtag : "",
+        sessiontitle :"GitOps and Best Practices for Cloud Native CI/CD",
+        filename : "/thumb/Antonio_Liccardi.png",
+        rebuild: true,
+        save: true
+    },
+    {
+        roomnumber : "1",
+
+        sessiontime: "10:30",
+        speakername : "Emanuele Bartolesi",
+        speakerrole : "ASP.NET Core Ninja",
+        sessionlanguage : "",
+        sessionhashtag : "",
+        sessiontitle :"Versionamento e pubblicazione di librerie con Git e GitHub Action",
+        filename : "/thumb/Emanuele_Bartolesi.png",
+        rebuild: true,
+        save: true
+    },
+    {
+        roomnumber : "2",
+        sessiontime: "10:30",
+        speakername : "Giancarlo Lelli",
+        speakerrole : "Microsoft Cloud Solution Architect for Application Innovation, Modernization & Enterprise 5G - Microsoft MVP @ Avanade",
+        sessionlanguage : "",
+        sessionhashtag : "",
+        sessiontitle :"GitOps con Azure Arc e Terraform a confronto",
+        filename : "/thumb/Giancarlo_Lelli.png",
+        rebuild: true,
+        save: true
+    },
+    {
+        roomnumber : "3",
+        sessiontime: "10:30",
+        speakername : "Giorgio Lasala",
+        speakerrole : "Engineering - Solution Architect",
+        sessionlanguage : "",
+        sessionhashtag : "",
+        sessiontitle :"Self Hosted Agent e l'attacco dei cloni",
+        filename : "/thumb/Giorgio_Lasala.png",
+        rebuild: true,
+        save: true
+    },
+
+    {
+        roomnumber : "4",
+        sessiontime: "10:30",
+        speakername : "Ian Victor Calcagni",
+        speakerrole : "Microsoft Certified DevOps Engineer Expert | Customer Engineer @Microsoft",
+        sessionlanguage : "",
+        sessionhashtag : "",
+        sessiontitle :"Automate your Power BI reports deployment using Azure DevOps and GitHub Actions",
+        filename : "/thumb/Ian_Victor_Calcagni.jpg",
+        rebuild: true,
+        save: true
+    },
+
+    {
+        roomnumber : "1",
+        sessiontime: "11:20",
+        speakername : "Ivan Porta",
+        speakerrole : "Microsoft Certified DevOps Engineer Expert | MCT | Public Speaker | Customer Engineer @Microsoft",
+        sessionlanguage : "",
+        sessionhashtag : "",
+        sessiontitle :"Automate your load testing using Azure DevOps, K6 and Log Analytics",
+        filename : "/thumb/Ivan_Porta.jpg",
+        rebuild: true,
+        save: true
+    },
+    {
+        roomnumber : "2",
+        sessiontime: "11:20",
+        speakername : "Jeff Wouters",
+        speakerrole : "MVP CDM | MCT | CTO @ Methos | JeffOps",
+        sessionlanguage : "",
+        sessionhashtag : "",
+        sessiontitle :"Generating your Azure DevOps extensions",
+        filename : "/thumb/Jeff_Wouters.jpg",
+        rebuild: true,
+        save: true
+    },
+    {
+        roomnumber : "3",
+        sessiontime: "11:20",
+        speakername : "Luis Beltran",
+        speakerrole : "Microsoft MVP, Xamarin Certified Mobile Developer",
+        sessionlanguage : "",
+        sessionhashtag : "",
+        sessiontitle :"CI/CD for Xamarin developers with GitHub Actions and AppCenter",
+        filename : "/thumb/Luis_Beltran.jpg",
+        rebuild: true,
+        save: true
+    },
+    {
+        roomnumber : "4",
+        sessiontime: "11:20",
+        speakername : "Marco Minerva",
+        speakerrole : "Microsoft MVP, Co-Founder of DotNetToscana and member of DotNetLiguria User Groups. Freelance Consultant. Passionate .NET Developer, Speaker & Writer.",
+        sessionlanguage : "",
+        sessionhashtag : "",
+        sessiontitle :"Versionamento e pubblicazione di librerie con Git e GitHub Action",
+        filename : "/thumb/Marco_Minerva.png",
+        rebuild: true,
+        save: true
+    },
+    
+    {
+        roomnumber : "1",
+        sessiontime: "12:10",
+        speakername : "Marco Obinu",
+        speakerrole : "Azure MVP, Advisory Engineer @ Softjam S.p.A, Geek to the bone",
+        sessionlanguage : "",
+        sessionhashtag : "",
+        sessiontitle :"Infrastructure as Code on Azure: Bicep & CI/CD pipelines",
+        filename : "/thumb/Marco_Obinu.jpg",
+        rebuild: true,
+        save: true
+    },
+    {
+        roomnumber : "2",
+        sessiontime: "12:10",
+        speakername : "Matteo Pagani",
+        speakerrole : "Windows App Consult Engineer @ Microsoft",
+        sessionlanguage : "",
+        sessionhashtag : "",
+        sessiontitle :"CI/CD per le applicazioni Windows con Azure DevOps, GitHub e MSIX ",
+        filename : "/thumb/Matteo_Pagani.jpg",
+        rebuild: true,
+        save: true
+    },
+    {
+        roomnumber : "3",
+        sessiontime: "12:10",
+        speakername : "Ram Iyengar",
+        speakerrole : "Developer Advocate at the Cloud Foundry Foundation",
+        sessionlanguage : "",
+        sessionhashtag : "",
+        sessiontitle :"Cloud Native CI/CD With Paketo Buildpacks & Tekton",
+        filename : "/thumb/Ram_Iyengar.png",
+        rebuild: true,
+        save: true
+    },
+    {
+        roomnumber : "4",
+        sessiontime: "12:10",
+        speakername : "Rob Richardson",
+        speakerrole : "@rob_rich",
+        sessionlanguage : "",
+        sessionhashtag : "",
+        sessiontitle :"Level-up Your DevOps with GitHub Actions and Kubernetes",
+        filename : "/thumb/Rob_Richardson.jpg",
+        rebuild: true,
+        save: true
+    },
+
+    {
+        roomnumber : "1",
+        sessiontime: "14:00",
+        speakername : "Roberto Freato",
+        speakerrole : "Solution Architect, Conference Speaker, Author (7 books), CTO @Witailer, Microsoft Azure MVP",
+        sessionlanguage : "",
+        sessionhashtag : "",
+        sessiontitle :"Scalable Azure WebJobs depoyments with Azure DevOps and hierarchical YAML",
+        filename : "/thumb/Roberto_Freato.jpg",
+        rebuild: true,
+        save: true
+    },
+    {
+        roomnumber : "2",
+        sessiontime: "14:00",
+        speakername : "Mike Martin",
+        speakerrole : "Msft technical evangelist",
+        sessionlanguage : "",
+        sessionhashtag : "",
+        sessiontitle :"Ride the Lightning : A little gem called Azure Storage Static Websites",
+        filename : "/thumb/Martin.jpg",
+        rebuild: true,
+        save: true
+    },
+    {
+        roomnumber : "3",
+        sessiontime: "14:00",
+        speakername : "Stefano Tempesta",
+        speakerrole : "Building Azure Confidential Computing @ Microsoft",
+        sessionlanguage : "",
+        sessionhashtag : "",
+        sessiontitle :"Azure MLOps: Bring your DevOps to Machine Learning",
+        filename : "/thumb/Stefano_Tempesta.jpg",
+        rebuild: true,
+        save: true
+    },
+    {
+        roomnumber : "4",
+        sessiontime: "14:00",
+        speakername : "Tommaso Stocchi",
+        speakerrole : "Microsoft - Customer Engineer",
+        sessionlanguage : "",
+        sessionhashtag : "",
+        sessiontitle :"Automate your load testing using Azure DevOps, K6 and Log Analytics",
+        filename : "/thumb/Tommaso_Stocchi.jpg",
+        rebuild: true,
+        save: true
+    },
+   
+    {
+        roomnumber : "1",
+        sessiontime: "14:50",
+        speakername : "Valentina Bucci",
+        speakerrole : "Avanade",
+        sessionlanguage : "",
+        sessionhashtag : "",
+        sessiontitle :"Azure DevOps Monitoring ",
+        filename : "/thumb/Valentina_Bucci.PNG",
+        rebuild: true,
+        save: true
+    }
+
+]
