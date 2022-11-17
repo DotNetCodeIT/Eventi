92fabb2dd5cc4f64a96faf9fac866201125e6f04
Luca Congiu <congiuluc@hotmail.com>
2022-11-18 00:48:26 +0100

    Update s3_1_createsponsorvideo.sh

--- A.I. Day/2022/banners/source/s3_1_createsponsorvideo.sh
+++ A.I. Day/2022/banners/source/s3_1_createsponsorvideo.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
+./createvideofromimage.sh -i ./sponsor/avanade/avanade1.png -o ./sponsor/avanade/avanade1.mp4
+./createvideofromimage.sh -i ./sponsor/avanade/avanade2.png -o ./sponsor/avanade/avanade2.mp4
+
 ./createvideofromimage.sh -i ./sponsor/porini/porini1.png -o ./sponsor/porini/porini1.mp4
 ./createvideofromimage.sh -i ./sponsor/porini/porini2.png -o ./sponsor/porini/porini2.mp4
 
-./createvideofromimage.sh -i ./sponsor/unikey/unikey1.jpg -o ./sponsor/unikey/unikey1.mp4
-./createvideofromimage.sh -i ./sponsor/unikey/unikey1.jpg -o ./sponsor/unikey/unikey2.mp4
-
 ./createvideofromimage.sh -i ./sponsor/elly/elly1.png -o ./sponsor/elly/elly1.mp4
 ./createvideofromimage.sh -i ./sponsor/elly/elly2.png -o ./sponsor/elly/elly2.mp4
+
+./createvideofromimage.sh -i ./sponsor/lobra/lobra1.jpg -o ./sponsor/lobra/lobra1.mp4
+./createvideofromimage.sh -i ./sponsor/lobra/lobra2.jpg -o ./sponsor/lobra/lobra2.mp4
+
+./createvideofromimage.sh -i ./sponsor/unikey/unikey1.jpg -o ./sponsor/unikey/unikey1.mp4
+./createvideofromimage.sh -i ./sponsor/unikey/unikey2.jpg -o ./sponsor/unikey/unikey2.mp4
