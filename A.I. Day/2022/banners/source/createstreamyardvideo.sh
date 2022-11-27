94bfb14f026d7a3a1bc62b545af2c694eefeaf3c
Sely2k <sely2k@gmail.com>
2022-11-27 14:23:50 +0100

    add aiday banner (molto molto sporca la situazione... giusto per backup)

--- A.I. Day/2022/banners/source/createstreamyardvideo.sh
+++ A.I. Day/2022/banners/source/createstreamyardvideo.sh
@@ -46,13 +46,11 @@ ffmpeg \
   -loop 1 -t 10 -i sponsor/elly/elly2.png \
   -loop 1 -t 10 -i sponsor/porini/porini1.png \
   -loop 1 -t 10 -i sponsor/porini/porini2.png \
-  -loop 1 -t 10 -i sponsor/lobra/lobra1.png \
-  -loop 1 -t 10 -i sponsor/lobra/lobra2.png \
   -loop 1 -t 10 -i sponsor/unikey/unikey1.jpg \
   -loop 1 -t 10 -i sponsor/unikey/unikey2.jpg \
   -i AzureDayMarkoCut.mpeg \
   -filter_complex "[0]scale=1920:1080[s0];[1]scale=1920:1080[s1];[2]scale=1920:1080[s2];[3]scale=1920:1080[s3];[4]scale=1920:1080[s4];[5]scale=1920:1080[s5];  \
-  [6]scale=1920:1080[s6];[7]scale=1920:1080[s7];[8]scale=1920:1080[s8];[9]scale=1920:1080[s9];[10]scale=1920:1080[s10];[11]scale=1920:1080[s11];[12]scale=1920:1080[s12];[13]scale=1920:1080[s13];[14]scale=1920:1080[s14];[15]scale=1920:1080[s15];[16]scale=1920:1080[s16];
+  [6]scale=1920:1080[s6];[7]scale=1920:1080[s7];[8]scale=1920:1080[s8];[9]scale=1920:1080[s9];[10]scale=1920:1080[s10];[11]scale=1920:1080[s11];[12]scale=1920:1080[s12];[13]scale=1920:1080[s13];
   [s0][s1]xfade=transition=pixelize:duration=1:offset=9,format=yuv420p[s01]; \
   [s01][s2]xfade=transition=pixelize:duration=1:offset=18,format=yuv420p[s02]; \
   [s02][s3]xfade=transition=pixelize:duration=1:offset=27,format=yuv420p[s03]; \
@@ -63,10 +61,7 @@ ffmpeg \
   [s07][s8]xfade=transition=pixelize:duration=1:offset=72,format=yuv420p[s08]; \
   [s08][s9]xfade=transition=pixelize:duration=1:offset=81,format=yuv420p[s09]; \
   [s09][s10]xfade=transition=pixelize:duration=1:offset=90,format=yuv420p[s010]; \
-  [s10][s11]xfade=transition=pixelize:duration=1:offset=99,format=yuv420p[s011]; \
-  [s11][s12]xfade=transition=pixelize:duration=1:offset=108,format=yuv420p[s012]; \
-  [s12][s13]xfade=transition=pixelize:duration=1:offset=117,format=yuv420p[s013]; \
-  [s13][s14]xfade=transition=pixelize:duration=1:offset=126,format=yuv420p[s014]; \
-  [s14][s15]xfade=transition=pixelize:duration=1:offset=135,format=yuv420p[s015]; \
-  [s15][s16]xfade=transition=pixelize:duration=1:offset=143,format=yuv420p[s016];" $parameterO
+  [s010][s11]xfade=transition=pixelize:duration=1:offset=99,format=yuv420p[s011]; \
+  [s011][s12]xfade=transition=pixelize:duration=1:offset=108,format=yuv420p[s012]; \
+  [s012][s13]xfade=transition=pixelize:duration=1:offset=117,format=yuv420p" $parameterO
 
