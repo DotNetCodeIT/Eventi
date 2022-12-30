09148bd634cc75e63ccd1fdea2d1ce9c8cf25ca8
Luca Congiu <congiuluc@hotmail.com>
2022-12-30 22:56:09 +0100

    Create createyoutubevideos.sh

---
+++ XMASDEV/2022/banners/Source/createyoutubevideos.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+helpFunction()
+{
+   echo ""
+   echo "Usage: $0 -i inputfilename"
+   echo -e "\t-i Input File name"
+   exit 1 # Exit script after printing help
+}
+
+while getopts "i:o:" opt
+do
+   case "$opt" in
+      i ) parameterI="$OPTARG" ;;
+      o ) parameterO="$OPTARG" ;;
+      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+   esac
+done
+
+# Print helpFunction in case parameters are empty
+if [ -z "$parameterI" ]  || [ -z "$parameterO" ]
+then
+   echo "Some or all of the parameters are empty";
+   helpFunction
+fi
+
+# Begin script in case all parameters are correct
+echo "$parameterI"
+echo "$parameterO"
+
+rm $parameterO
+# ffmpeg -loop 1 -i $parameterI -c:v libx264 -t 15 -pix_fmt yuv420p -vf scale=1920:1080 $parameterO
+
+rm $parameterO
+
+ffmpeg \
+  -loop 1 -t 10 -i $parameterI \
+  -loop 1 -t 10 -i ./sponsor-slide.png \
+  -loop 1 -t 10 -i generic/track.png \
+  -i audio.mp3 \
+  -filter_complex "[0]scale=1920:1080[s0];[1]scale=1920:1080[s1];[2]scale=1920:1080[s2];
+  [s0][s1]xfade=transition=pixelize:duration=1:offset=9,format=yuv420p[s01]; \
+  [s01][s2]xfade=transition=pixelize:duration=1:offset=18,format=yuv420p[s02]; \
+  [s02][s3]xfade=transition=pixelize:duration=1:offset=27,format=yuv420p[s03]; \
+   $parameterO
