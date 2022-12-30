8ab0cce01c28cea076fc1438ede61f08acde412a
Luca Congiu <congiuluc@hotmail.com>
2022-12-31 00:10:59 +0100

    Update createaudiofile.sh

--- XMASDEV/2022/banners/Source/createaudiofile.sh
+++ XMASDEV/2022/banners/Source/createaudiofile.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+defaultFilename=output.mp3
+filename=${1:-$defaultFilename}
 
-ffmpeg -t 136 -i AzureDayMarko.mpeg -af "afade=type=in:start_time=0:duration=5" -c:a libmp3lame audio.mp3
+ffmpeg -t 136 -y -i AzureDayMarko.mpeg -af "afade=type=in:start_time=0:duration=5" -c:a libmp3lame $filename
