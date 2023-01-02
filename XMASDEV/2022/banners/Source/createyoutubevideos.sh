39bd45d4b6fa33a0443702bbb05d743494e2d8bc
Luca Congiu <congiuluc@hotmail.com>
2023-01-02 02:57:01 +0100

    Update createyoutubevideos.sh

--- XMASDEV/2022/banners/Source/createyoutubevideos.sh
+++ XMASDEV/2022/banners/Source/createyoutubevideos.sh
@@ -15,8 +15,6 @@ fi
  
 json=$(cat $config)
 
-
-
 while read youtube; do
    roomId=$(jq -r '.roomId' <<< "$youtube")
    echo $roomId
@@ -24,26 +22,39 @@ while read youtube; do
    echo $url
    yt-dlp --downloader aria2c -S res,ext:mp4:m4a --recode mp4  $url -o "$roomId.mp4"
 
-   
+
 done <<< $(jq -c '.youtubelive[]' $config)
 
-arr=()
+video=()
+videodata=()
 while read session; do
    title=$(jq -r '.title' <<< "$session")
-   echo $title
+   #echo $title
    description=$(jq -r '.description' <<< "$session")
-   echo $description
+   #echo $description
    roomId=$(jq -r '.roomId' <<< "$session")
    sessionId=$(jq -r '.id' <<< "$session")
    liveStart=$(jq -r '.liveStartsAt' <<< "$session")
    liveEnd=$(jq -r '.liveEndsAt' <<< "$session")
    echo ffmpeg -ss $liveStart -to $liveEnd -i "$roomId.mp4" -c copy "$sessionId.mp4" -y
-   arr+=(echo ffmpeg -ss $liveStart -to $liveEnd -i "$roomId.mp4" -c copy "$sessionId.mp4" -y)
-   #ffmpeg -ss $liveStart -to $liveEnd -i "$roomId.mp4" -c copy "$sessionId.mp4" -y
-   #sleep 5s # Waits 5 seconds.
+   video+=("ffmpeg -ss $liveStart -to $liveEnd -i ""$roomId.mp4"" -c copy ""$sessionId.mp4"" -y")
+   echo "{\"title\": \"$title\",\"description\":\"$description\"}" > "$sessionId.json"
+  #echo $json_data
+  #echo $json_data > $sessionId.json
+  #videodata+=("$json_data")
+   videodata+=("{\"title\": \"$title\",\"description\":\"$description\"}")
+  #videodata+=("$json_data")
 done <<< $(jq -c '.sessions[]' $config)
-echo ${arr[@]}
+#echo ${video[@]}
+for i in ${!video[@]} # "${video[@]}"
+do
+   ${video[$i]}
+   echo ${videodata[$i]}
+   # or do whatever with individual element of the array
+done
+
 json=$(cat $config)
+
 
 readJsonConfig() {
         echo $json | jq -r $1
