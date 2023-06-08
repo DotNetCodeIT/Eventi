f014ec20d04d3383c8dcba7b333e727d1def418f
Sely2k <sely2k@gmail.com>
2023-06-08 09:21:59 +0200

    add video for .net conf 2023

--- dotNetConf/2023/banners/video/Banner/createaudiofile.sh
+++ dotNetConf/2023/banners/video/Banner/createaudiofile.sh
@@ -2,4 +2,4 @@
 defaultFilename=audio.mp3
 filename=${1:-$defaultFilename}
 
-ffmpeg -t 109 -y -i AzureDayMarko.mpeg -af "afade=type=in:start_time=0:duration=5,afade=t=out:st=104:d=5" -c:a libmp3lame $filename
+ffmpeg -t 127 -y -i AzureDayMarko.mpeg -af "afade=type=in:start_time=0:duration=5,afade=t=out:st=122:d=5" -c:a libmp3lame $filename
--- dotNetConf/2023/banners/video/Banner/createstreamyardvideo.sh
+++ dotNetConf/2023/banners/video/Banner/createstreamyardvideo.sh
@@ -36,11 +36,6 @@ rm $parameterO
 ffmpeg \
   -loop 1 -t 7 -i $parameterI \
   -loop 1 -t 7 -i ./sponsor-slide.png \
-  -loop 1 -t 7 -i generic/track.png \
-  -loop 1 -t 7 -i generic/attestato.png \
-  -loop 1 -t 7 -i generic/selfielottery.png \
-  -loop 1 -t 7 -i generic/mobileApp.png \
-  -loop 1 -t 7 -i generic/sondaggio.png \
   -loop 1 -t 7 -i sponsor/sponsor1/sponsor1_1.png \
   -loop 1 -t 7 -i sponsor/sponsor1/sponsor1_2.png \
   -loop 1 -t 7 -i sponsor/sponsor2/sponsor2_1.png \
@@ -51,11 +46,19 @@ ffmpeg \
   -loop 1 -t 7 -i sponsor/sponsor4/sponsor4_2.png \
   -loop 1 -t 7 -i sponsor/sponsor5/sponsor5_1.png \
   -loop 1 -t 7 -i sponsor/sponsor5/sponsor5_2.png \
+  -loop 1 -t 7 -i sponsor/sponsor6/sponsor6_1.png \
+  -loop 1 -t 7 -i sponsor/sponsor6/sponsor6_2.png \
+  -loop 1 -t 7 -i generic/track.png \
+  -loop 1 -t 7 -i generic/selfielottery.png \
+  -loop 1 -t 7 -i generic/mobileApp.png \
+  -loop 1 -t 7 -i generic/sondaggio.png \
+  -loop 1 -t 7 -i generic/attestato.png \
   -loop 1 -t 7 -i ./sponsor-slide-end.png \
+  -loop 1 -t 7 -i ./sponsor-slide.png \
   -i audio.mp3 \
-  -filter_complex "[0]scale=1920:1080[s0];[1]scale=1920:1080[s1];[2]scale=1920:1080[s2];[3]scale=1920:1080[s3];[4]scale=1920:1080[s4];[5]scale=1920:1080[s5];  \
+  -filter_complex "[0]scale=1920:1080[s0];[1]scale=1920:1080[s1];[2]scale=1920:1080[s2];[3]scale=1920:1080[s3];[4]scale=1920:1080[s4];[5]scale=1920:1080[s5]; \
   [6]scale=1920:1080[s6];[7]scale=1920:1080[s7];[8]scale=1920:1080[s8];[9]scale=1920:1080[s9];[10]scale=1920:1080[s10];[11]scale=1920:1080[s11];[12]scale=1920:1080[s12]; \
-  [13]scale=1920:1080[s13];[14]scale=1920:1080[s14];[15]scale=1920:1080[s15];[16]scale=1920:1080[s16];[17]scale=1920:1080[s17];
+  [13]scale=1920:1080[s13];[14]scale=1920:1080[s14];[15]scale=1920:1080[s15];[16]scale=1920:1080[s16];[17]scale=1920:1080[s17];[18]scale=1920:1080[s18];[19]scale=1920:1080[s19];[20]scale=1920:1080[s20];
   [s0][s1]xfade=transition=pixelize:duration=1:offset=6,format=yuv420p[s01]; \
   [s01][s2]xfade=transition=pixelize:duration=1:offset=12,format=yuv420p[s02]; \
   [s02][s3]xfade=transition=pixelize:duration=1:offset=18,format=yuv420p[s03]; \
@@ -65,12 +68,14 @@ ffmpeg \
   [s06][s7]xfade=transition=pixelize:duration=1:offset=42,format=yuv420p[s07]; \
   [s07][s8]xfade=transition=pixelize:duration=1:offset=48,format=yuv420p[s08]; \
   [s08][s9]xfade=transition=pixelize:duration=1:offset=54,format=yuv420p[s09]; \
-  [s09][s10]xfade=transition=pixelize:duration=1:offset=60,format=yuv420p[s010]; \
-  [s010][s11]xfade=transition=pixelize:duration=1:offset=66,format=yuv420p[s011]; \
-  [s011][s12]xfade=transition=pixelize:duration=1:offset=72,format=yuv420p[s012]; \
-  [s012][s13]xfade=transition=pixelize:duration=1:offset=78,format=yuv420p[s013]; \
-  [s013][s14]xfade=transition=pixelize:duration=1:offset=84,format=yuv420p[s014]; \
-  [s014][s15]xfade=transition=pixelize:duration=1:offset=90,format=yuv420p[s015]; \
-  [s015][s16]xfade=transition=pixelize:duration=1:offset=96,format=yuv420p[s016]; \
-  [s016][s17]xfade=transition=pixelize:duration=1:offset=102,format=yuv420p" $parameterO
-
+  [s09][s10]xfade=transition=pixelize:duration=1:offset=60,format=yuv420p[s10]; \
+  [s10][s11]xfade=transition=pixelize:duration=1:offset=66,format=yuv420p[s11]; \
+  [s11][s12]xfade=transition=pixelize:duration=1:offset=72,format=yuv420p[s12]; \
+  [s12][s13]xfade=transition=pixelize:duration=1:offset=78,format=yuv420p[s13]; \
+  [s13][s14]xfade=transition=pixelize:duration=1:offset=84,format=yuv420p[s14]; \
+  [s14][s15]xfade=transition=pixelize:duration=1:offset=90,format=yuv420p[s15]; \
+  [s15][s16]xfade=transition=pixelize:duration=1:offset=96,format=yuv420p[s16]; \
+  [s16][s17]xfade=transition=pixelize:duration=1:offset=102,format=yuv420p[s17]; \
+  [s17][s18]xfade=transition=pixelize:duration=1:offset=108,format=yuv420p[s18]; \
+  [s18][s19]xfade=transition=pixelize:duration=1:offset=114,format=yuv420p[s19]; \
+  [s19][s20]xfade=transition=pixelize:duration=1:offset=120,format=yuv420p" $parameterO
